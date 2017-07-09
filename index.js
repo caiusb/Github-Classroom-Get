5d6fcb01978c8102e60ccf2d03cd5d397e29a342
Caius Brindescu <brindesc@oregonstate.edu>
2017-07-08 19:03:52 -0700

    First working version

---
+++ index.js
@@ -0,0 +1,64 @@
+const request = require('request')
+const fs = require('fs');
+const parseLH = require('parse-link-header');
+const spawn = require('child_process').spawnSync;
+
+var argv = require('minimist')(process.argv.slice(2));
+
+var org = argv['org'];
+var user = argv['user'];
+var token = argv['token'];
+var save = argv['save'];
+var assignment = argv['assignment'];
+
+var options = {
+  url: 'https://api.github.com/orgs/' + org + '/repos',
+  headers: {
+    'User-Agent': user,
+    'per-page': 100
+  }
+}
+
+request(options, doResponse([])).auth(user, token, true);
+
+function doResponse(repos) {
+  return function(err, res, body) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    if (res.statusCode == 404) {
+      console.log("Could not find organization");
+      process.exit(1);
+    }
+    var newRepos = JSON.parse(body);
+    var allRepos = repos.concat(newRepos);
+    if (res.headers['link']) {
+      var links = parseLH(res.headers['link']);
+      if (links['next']) {
+        options['url'] = links['next']['url'];
+        request(options, doResponse(allRepos)).auth(user, token, true);
+      } else {
+        cloneAssignments(allRepos);
+        process.exit(0);
+      }
+    } else {
+      cloneAssignments(allRepos);
+    }
+  }
+}
+
+function cloneAssignments(repos) {
+  var assignmentRepos = repos.filter((repo) => {
+    return repo['name'].startsWith(assignment);
+  });
+  console.log(assignmentRepos);
+  var assignmentDir = __dirname + '/' + assignment;
+  if (!fs.existsSync(assignmentDir))
+    fs.mkdirSync(assignmentDir);
+  process.chdir(assignmentDir);
+  for (var a of assignmentRepos) {
+    console.log("Cloning " + a['ssh_url']);
+    var git = spawn('git', ['clone', a['ssh_url']]);
+  }
+}
